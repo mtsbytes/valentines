0101bb3a72b95a1bbc2e3cc398c2b5eb43007f35
mtsbyts <thawfiqsuhail5@gmail.com>
2026-02-01 21:26:21 +0530

    Fix image paths for deployment 4

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ const nextConfig: NextConfig = {
   // Your Next.js config
   output: 'export', // This is required for GitHub Pages
   basePath: '/valentines', // Replace 'your-repo-name' with your actual GitHub repo name
+  assetPrefix: '/valentines/',
   images: {
     unoptimized: true, // Required because GitHub Pages doesn't support Next.js Image Optimization
   },
