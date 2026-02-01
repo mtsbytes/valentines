0a51250b3d627de2406f6eeab1c99aa74d154c22
mtsbyts <thawfiqsuhail5@gmail.com>
2026-02-01 20:20:18 +0530

    Initial commit of my personalized valentine site

---
+++ next.config.ts
@@ -0,0 +1,12 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  // Your Next.js config
+  output: 'export', // This is required for GitHub Pages
+  basePath: '/valentines', // Replace 'your-repo-name' with your actual GitHub repo name
+  images: {
+    unoptimized: true, // Required because GitHub Pages doesn't support Next.js Image Optimization
+  },
+};
+
+export default nextConfig;
